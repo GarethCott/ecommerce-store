ba6ade001eff45931ca4cf0e103dabfc15d2b209
Gareth Cottrell <gareth.cott@gmail.com>
2023-07-14 10:53:56 +0200

    Add billboards to homepage from backend API route

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,18 @@
-const HomePage = () => {
+import getBillboards from "@/actions/git-billboards";
+import Billboard from "@/components/Billboard"
+import Container from "@/components/ui/Container"
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+  const billboards = await getBillboards("bfc6670d-a5f5-4a31-b501-6ab5e3688551");
   return (
-    <div>Hello Store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+    	  <Billboard data={billboards}/>
+      </div>
+    </Container>
   )
 }
 export default HomePage
