cdb54dc1e7d4979abf800ed871f0c78aa2687964
GarethCott <gareth.cott@gmail.com>
2023-07-17 22:22:18 +0200

    Make add to card button work on modal

--- components/Info.tsx
+++ components/Info.tsx
@@ -5,6 +5,7 @@ import { FC } from 'react'
 import Currency from './ui/Currency'
 import Button from './ui/Button'
 import { ShoppingCart } from 'lucide-react'
+import useCart from '@/hooks/useCart'
 
 interface InfoProps {
   data: Product
@@ -13,7 +14,14 @@ interface InfoProps {
 const Info: FC<InfoProps> = ({
     data,
 }) => {
+
+    const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  }
   return (
+
     <div>
         <h1 className='text-3xl font-bold text-gray-900'>
             {data.name}
@@ -37,7 +45,7 @@ const Info: FC<InfoProps> = ({
             </div>
         </div>
         <div className='mt-10 flex items-center gap-x-3'>
-            <Button className='flex items-center gap-x-2'>
+            <Button onClick={onAddToCart} className='flex items-center gap-x-2'>
                 Add To Cart
                 <ShoppingCart/>
             </Button>
