686b49a116e6da6c5c35009dce742f5d33e9c73d
Gareth Cottrell <gareth.cott@gmail.com>
2023-07-17 13:45:30 +0200

    Finish with Product modal

--- components/PreviewModal.tsx
+++ components/PreviewModal.tsx
@@ -2,6 +2,9 @@
 
 import usePreviewModal from '@/hooks/usePreviewModel'
 import { FC } from 'react'
+import Modal from './ui/Modal'
+import Gallery from './gallery'
+import Info from './Info'
 
 interface PreviewModalProps {
   
@@ -17,7 +20,16 @@ const PreviewModal: FC<PreviewModalProps> = ({}) => {
     }
 
   return (
-    <MOdal
+    <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
+      <div className='grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8'>
+        <div className='sm:col-span-4 lg:col-span-5'>
+          <Gallery images={product.images}/>
+        </div>
+        <div className='sm:col-span-8 lg:col-span-7'>
+          <Info data={product}/>
+        </div>
+      </div>
+    </Modal>
   )
 }
 
--- components/ui/ProdcutCard.tsx
+++ components/ui/ProdcutCard.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { Product } from '@/types'
 import Image from 'next/image';
-import { FC } from 'react'
+import { FC, MouseEventHandler } from 'react'
 import IconButton from './IconButton';
 import { Expand, ShoppingCart } from 'lucide-react';
 import Currency from './Currency';
 import { useRouter } from 'next/navigation';
+import usePreviewModal from '@/hooks/usePreviewModel';
 
 interface ProdcutCardProps {
   data:Product;
@@ -14,19 +15,26 @@ interface ProdcutCardProps {
 const ProdcutCard: FC<ProdcutCardProps> = ({
     data
 }) => {
+    const previewModal = usePreviewModal();
     const router = useRouter();
     const handleClick = () => {
         router.push(`/product/${data?.id}`)
     }
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data)
+    }
   return (
-    <div className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
+    <div onClick={handleClick} className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
         {/* Images and actions */}
         <div className='aspect-square rounded-xl bg-gray-100 relative'>
             <Image alt='image' src={data?.images[0]?.url} fill className='aspect-square object-cover rounded-md'/>
             <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
                 <div className='flex gap-x-6 justify-center'>
                     <IconButton 
-                    onClick={handleClick}
+                    onClick={onPreview}
                     icon={<Expand size={20} className='text-gray-600'/>}
                     />
                     <IconButton 
