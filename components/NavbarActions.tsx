909b7c4fbc5ef4a1eb114de0b183a73abfd16720
Gareth Cottrell <gareth.cott@gmail.com>
2023-07-17 14:14:31 +0200

    Add cart page and modal

--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect, useState } from 'react'
 import Button from './ui/Button'
 import { ShoppingBag } from 'lucide-react'
 import useCart from '@/hooks/useCart'
+import { useRouter } from 'next/navigation'
 
 interface NavbarActionsProps {
   
@@ -17,6 +18,7 @@ const NavbarActions: FC<NavbarActionsProps> = ({}) => {
         setIsMounted(true)
     }, [])
 
+    const router = useRouter();
     const cart = useCart();
 
     if(!isMounted) {
@@ -25,7 +27,7 @@ const NavbarActions: FC<NavbarActionsProps> = ({}) => {
 
   return (
     <div className='ml-auto flex items-center gap-x-4'>
-        <Button className='flex items-center rounded-full bg-black px-4 py-2'>
+        <Button onClick={()=> router.push("/cart")} className='flex items-center rounded-full bg-black px-4 py-2'>
             <ShoppingBag size={20} color='white'/>
             <span className='ml-2 text-sm font-medium text-white'>
                 {cart.items.length}
