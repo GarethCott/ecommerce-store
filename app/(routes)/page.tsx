234cc2cd20d879e926a26f89a543c89a44425add
Gareth Cottrell <gareth.cott@gmail.com>
2023-07-19 09:56:09 +0200

    Chnage front page billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,24 +1,25 @@
 import getBillboards from "@/actions/git-billboards";
 import getProducts from "@/actions/git-products";
-import Billboard from "@/components/Billboard"
+import Billboard from "@/components/Billboard";
 import ProductList from "@/components/ProductList";
-import Container from "@/components/ui/Container"
+import Container from "@/components/ui/Container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-
-  const products = await getProducts({isFeatured: true});
-  const billboards = await getBillboards("968ffd57-82be-4512-8db0-eb8798f0be23");
+  const products = await getProducts({ isFeatured: true });
+  const billboards = await getBillboards(
+    "86e124ef-18a6-43af-b667-8cc45db3afaf"
+  );
   return (
     <Container>
-     <div className="space-y-10 pb-10">
-    	  <Billboard data={billboards}/>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboards} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-            <ProductList title="Featured Products" items={products}/>
+          <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
